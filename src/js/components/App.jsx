fada87632578c132be698b98102888ac13aaf7e5
Jirawong Wongdokpuang <jirawong.wdp@gmail.com>
2015-12-15 20:58:47 +0700

    add login path

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import Header from '../components/header/Header';
 
-class App extends React.Component {
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this._getLoginState();
+    }
+
+    _getLoginState() {
+        return {'isLoggedIn': true};
+    }
+
     render() {
-        return (
-            <div id='app-component'>
-                <Header />
-                {this.props.children}
-            </div>
-        );
+        if (this.state.isLoggedIn) {
+            return (
+                <div id='app-component'>
+                    <Header />
+                    {this.props.children}
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    Login Page here.
+                </div>
+            );
+        }
     }
 }
-
-export default App;
