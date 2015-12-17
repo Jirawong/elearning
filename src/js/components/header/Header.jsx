03ad4be58d653ec79eeed745924679a9e948cbb7
Jirawong Wongdokpuang <Kong@Jirawongs-iMac.local>
2015-12-18 06:40:44 +0700

    test bootstrap header

--- src/js/components/header/Header.jsx
+++ src/js/components/header/Header.jsx
@@ -1,8 +1,6 @@
 import './header.scss';
 
 import React from 'react';
-import Button from '../button/Button';
-import Search from '../search/Search';
 
 import AuthenService from '../../services/AuthenService';
 
@@ -15,13 +13,43 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="header">
-                <div className="header-inner flex">
-                    <Button icon="bars" onClick={this.logout.bind(this)}>Browse Courses</Button>
-                    <Search />
-                    e-Learning
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                    </div>
+
+                    <div className="collapse navbar-collapse">
+
+                        <ul className="nav navbar-nav navbar-left">
+                            <li><button type="button" className="btn btn-default navbar-btn">Browse Courses</button></li>
+                        </ul>
+                        <form className="navbar-form navbar-left" role="search">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Search" />
+                            </div>
+                        </form>
+
+                        <ul className="nav navbar-nav navbar-right">
+                            <li role="presentation" className="dropdown">
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span className="caret"></span></a>
+                                <ul className="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li><a href="#">Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
         );
     }
 }
