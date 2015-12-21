418325cab1da520649fc539e125815ee0946802c
Jirawong Wongdokpuang <Kong@Jirawongs-iMac.local>
2015-12-22 03:36:41 +0700

    fix page not found when pushStash

--- src/main/java/com/linksinnovation/elearning/Application.java
+++ src/main/java/com/linksinnovation/elearning/Application.java
@@ -2,12 +2,28 @@ package com.linksinnovation.elearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author Jirawong Wongdokpuang <jirawong@linksinnovation.com>
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+                configurableEmbeddedServletContainer
+                        .addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
